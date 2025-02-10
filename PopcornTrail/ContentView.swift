3467237349f4230ad8a3db31a5a5599ffb956ad9
Rubén <ruben.sr.apple@gmail.com>
2025-02-10 18:28:09 +0100

    Init project

--- PopcornTrail/ContentView.swift
+++ PopcornTrail/ContentView.swift
@@ -10,10 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+			Image(systemName: "popcorn.fill")
+				.resizable()
+				.scaledToFit()
+				.frame(width: 50)
+				.foregroundStyle(.red)
+            Text("🔎 Keep track of the best content")
+				.font(.title3)
+				.fontWeight(.semibold)
+				.fontDesign(.rounded)
+				.foregroundStyle(.white)
+				.background(
+					LinearGradient(colors: [.red, .pink, .orange, .yellow, .mint, .green], startPoint: .leading, endPoint: .trailing)
+						.opacity(0.7)
+				)
+				.clipShape(RoundedRectangle(cornerRadius: 5))
+				
         }
         .padding()
     }
