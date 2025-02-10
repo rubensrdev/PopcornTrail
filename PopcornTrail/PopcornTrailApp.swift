f0e1ab688a2d3af9f3da483a75af91da300e658a
Rubén <ruben.sr.apple@gmail.com>
2025-02-10 17:58:54 +0100

    Initial Commit

---
+++ PopcornTrail/PopcornTrailApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopcornTrailApp.swift
+//  PopcornTrail
+//
+//  Created by Rubén Segura Romo on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct PopcornTrailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
