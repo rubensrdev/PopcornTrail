308b5bc4b1ebcba4cf9ef119fabee4a844e0ffe3
Rubén <ruben.sr.apple@gmail.com>
2025-02-21 12:17:40 +0100

    Add enum para movies

---
+++ PopcornTrail/Network/Enum/MoviesEndpoints.swift
@@ -0,0 +1,12 @@
+//
+//  MoviesEndpoints.swift
+//  PopcornTrail
+//
+//  Created by Rubén Segura Romo on 21/2/25.
+//
+
+import Foundation
+
+enum MoviesEndpoints: String {
+	case popular = "/movie/popular"
+}
