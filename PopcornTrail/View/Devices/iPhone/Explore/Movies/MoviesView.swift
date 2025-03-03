ff30767c99fac0ca2ee9468130ba09633afc5002
Rubén <ruben.sr.apple@gmail.com>
2025-03-03 12:00:25 +0100

    Maquentando la vista de popular moview

--- PopcornTrail/View/Devices/iPhone/Explore/Movies/MoviesView.swift
+++ PopcornTrail/View/Devices/iPhone/Explore/Movies/MoviesView.swift
@@ -41,6 +41,7 @@ struct MoviesView: View {
 														.scaledToFill()
 														.frame(width: 150, height: 225)
 														.clipShape(RoundedRectangle(cornerRadius: 12))
+														.shadow(color: .black.opacity(0.6) ,radius: 8, x: 0, y: 4)
 												} placeholder: {
 													ProgressView()
 														.frame(width: 150, height: 225)
@@ -52,11 +53,13 @@ struct MoviesView: View {
 													.frame(width: 100, height: 150)
 													.foregroundColor(.gray.opacity(0.7))
 											}
+											
 											Text(movie.title)
 												.font(.headline)
 												.fontDesign(.rounded)
 												.multilineTextAlignment(.center)
 												.frame(width: 100)
+											
 										}
 										.padding(8)
 									}
