2101b3f17f0c4be31e1ba594dee35eb9bdcdb859
Rubén <ruben.sr.apple@gmail.com>
2025-03-04 11:02:05 +0100

    Alñado ficheros con datos mockeados para la preview

--- PopcornTrail/Preview Content/MovieRepositoryPreview.swift
+++ PopcornTrail/Preview Content/MovieRepositoryPreview.swift
@@ -9,8 +9,40 @@ import Foundation
 
 /// Repositorio de películas para previsualización en SwiftUI
 struct MovieRepositoryPreview: MovieRepositoryProtocol {
+	func getTopRatedMovies() async throws -> [Movie] {
+		guard let url = Bundle.main.url(forResource: "TopRatedMovies", withExtension: "json") else {
+			throw NetworkError.dataNotValid
+		}
+		
+		let data = try Data(contentsOf: url)
+		let response = try JSONDecoder().decode(MovieResponse.self, from: data)
+		
+		return response.results
+	}
 	
-	/// Carga las películas populares desde un archivo JSO
+	func getNowPlayingMovies() async throws -> [Movie] {
+		guard let url = Bundle.main.url(forResource: "NowPlayingMovies", withExtension: "json") else {
+			throw NetworkError.dataNotValid
+		}
+		
+		let data = try Data(contentsOf: url)
+		let response = try JSONDecoder().decode(MovieResponse.self, from: data)
+		
+		return response.results
+	}
+	
+	func getUpcomingMovies() async throws -> [Movie] {
+		guard let url = Bundle.main.url(forResource: "UpcomingMovies", withExtension: "json") else {
+			throw NetworkError.dataNotValid
+		}
+		
+		let data = try Data(contentsOf: url)
+		let response = try JSONDecoder().decode(MovieResponse.self, from: data)
+		
+		return response.results
+	}
+	
+	/// Carga las películas populares desde un archivo JSON
 	func getPopularMovies() async throws -> [Movie] {
 		guard let url = Bundle.main.url(forResource: "PopularMovies", withExtension: "json") else {
 			throw NetworkError.dataNotValid
