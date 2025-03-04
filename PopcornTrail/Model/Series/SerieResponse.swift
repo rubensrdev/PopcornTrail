bb08af6e28febd325a0a7bd827f4e8e0b0854a4f
Rubén <ruben.sr.apple@gmail.com>
2025-03-04 11:35:15 +0100

    add comentario documentado

--- PopcornTrail/Model/Series/SerieResponse.swift
+++ PopcornTrail/Model/Series/SerieResponse.swift
@@ -8,6 +8,13 @@
 import Foundation
 
 /// Representa la respuesta de la API de TMDb al solicitar una lista de series de televisión.
+///
+/// - Propiedades:
+///   - `page`: Número de página de la respuesta actual.
+///   - `results`: Lista de series de televisión obtenidas en la consulta.
+///   - `totalResults`: Número total de resultados disponibles en la API.
+///   - `totalPages`: Número total de páginas disponibles en la API.
+///
 struct SerieResponse: Codable {
 	let page: Int
 	let results: [Serie]
